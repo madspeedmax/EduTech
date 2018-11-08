921d399b0aa579e8a25db5b8387970e2b5b0ba48
madspeedmax <hosokawa13@gmail.com>
2018-11-07 22:19:46 -0800

    basic assess edit

---
+++ StudyReg.Web/Pages/Goals/Assess.cshtml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudyReg.Web.Areas.Identity.Data;
+using StudyReg.Web.Data;
+using StudyReg.Web.Models;
+
+namespace StudyReg.Web.Pages.Goals
+{
+    public class AssessModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<StudyRegWebUser> _userManager;
+
+        public AssessModel(ApplicationDbContext context, UserManager<StudyRegWebUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public Goal Goal { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            Goal = await _context.Goal
+                .FirstOrDefaultAsync(
+                    m => m.Id == id && 
+                    m.User.Id == user.Id && 
+                    m.GoalDate.Date < DateTime.UtcNow.Date);
+
+            if (Goal == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (!ModelState.IsValid && Goal.GoalDate.Date < DateTime.UtcNow.Date && Goal.User.Id == user.Id)
+            {
+                return Page();
+            }
+
+            Goal.SelfAssessmentDate = DateTime.UtcNow;
+            _context.Attach(Goal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GoalExists(Goal.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool GoalExists(int id)
+        {
+            return _context.Goal.Any(e => e.Id == id);
+        }
+    }
+}
